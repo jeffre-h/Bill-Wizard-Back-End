20de32b9d1f95687aee7969db9ab6e03f5cdb3c8
Ronney Lok <ronneylok@Ronneys-MacBook-Pro.local>
2023-05-11 15:37:15 -0700

    added testing code for createReceipt image feature

--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -31,7 +31,38 @@ void createReceipt() async {
   print(response.body);
 }
 
+// Function for adding receipt.
+Future<void> sendRequestWithFile() async {
+  var request = http.MultipartRequest(
+      'POST', Uri.parse('http://localhost:3000/api/createReceipt'));
+
+  // Add the file to the request
+  // UNCOMMENT THIS WHEN TESTING
+  // var file = await http.MultipartFile.fromPath('image', );
+  // request.files.add(file);
+
+  // Add additional fields
+  request.fields['location'] = "Boston Pizza";
+  request.fields['when'] = "March 20 1993";
+  request.fields['subTotal'] = "22.21";
+  request.fields['tax'] = "2.32";
+  request.fields['tip'] = "1.23";
+  request.fields['payerEmail'] = "kevindang@gmail.com";
+  // request.fields['image'] = imgPath;
+
+  // Send the request
+  var response = await request.send();
+
+  // Handle the response
+  if (response.statusCode == 200) {
+    print('Request successful');
+  } else {
+    print('Request failed with status code: ${response.statusCode}');
+  }
+}
+
 void main() async {
   createUser();
   createReceipt();
+  sendRequestWithFile();
 }
